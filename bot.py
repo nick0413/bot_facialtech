44e21c93e35ab262ce21ff9d1500b78857a95d83
nick0413 <68749910+nick0413@users.noreply.github.com>
2023-07-11 12:32:20 -0500

    bot funcional

--- bot.py
+++ bot.py
@@ -13,7 +13,10 @@ from os.path import exists
 today = date.today()
 
 # dd/mm/YY
-d1 = today.strftime("%m/%d/%Y")
+fecha = today.strftime("%d/%m/%Y")
+d1=today.strftime("%m/%d/%Y")
+fecha='12/07/2023'
+d1='12/07/2023'
 # print("d1 =", d1.replace('/','-'))
 
 
@@ -23,66 +26,117 @@ password='1018472666123'
 
 driver = webdriver.Chrome()
 actions = ActionChains(driver) 
-driver.get(pagina)
+def get_row(d1,fecha):
+	driver.get(pagina)
+
+	WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="usuario"]'))).send_keys(correo)
+	WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="clave"]'))).send_keys(password)
+
+	actions.send_keys(Keys.RETURN)
+	actions.perform()
+	actions.reset_actions()
+
+	WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="estadisticas"]/a/span'))).click()
+	WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="fecha_inicio"]'))).send_keys(d1)
+	WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="fecha_final"]'))).send_keys(d1)
+	WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="frmFechasEstadisticas"]/section[2]/section/button'))).click()
+
+	time.sleep(1)
 
 
-WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="usuario"]'))).send_keys(correo)
+	ventas=			driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[1]/label[2]').text
+	efectivo=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[3]/label[2]').text
+	Datafono=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[4]/label[2]').text
+	Transferencia=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[5]/label[2]').text
+	Egresos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[2]/label[2]').text
+	Descuentos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[6]/label[2]').text
+	Bancos= 		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[7]/label[2]').text
+	efectivo_egre= 	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[8]/label[2]').text
+	T_bancos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[9]/label[2]').text
+	T_efectivo=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[10]/label[2]').text
+	Facebook= 		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[11]/label[2]').text
+	Google=			driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[12]/label[2]').text
+	Pasando=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[13]/label[2]').text
+	Instagram=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[14]/label[2]').text
+	Recomendado=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[15]/label[2]').text
+	Link_agenda=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[16]/label[2]').text
+	Sin_fuente=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[17]/label[2]').text
+	C_perdidos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[18]/label[2]').text
+	C_recurrentes=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[19]/label[2]').text
+	Cumple=			driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[20]/label[2]').text
 
-WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="clave"]'))).send_keys(password)
+	driver.quit()
 
-actions.send_keys(Keys.RETURN)
-actions.perform()
-actions.reset_actions()
-
-
-WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="estadisticas"]/a/span'))).click()
-
-
-WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="fecha_inicio"]'))).send_keys(d1)
-
-
-WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="fecha_final"]'))).send_keys(d1)
-
-
-WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="frmFechasEstadisticas"]/section[2]/section/button'))).click()
-
-time.sleep(1)
-
-ventas=			driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[1]/label[2]').text
-efectivo=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[3]/label[2]').text
-Datafono=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[4]/label[2]').text
-Transferencia=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[5]/label[2]').text
-Egresos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[2]/label[2]').text
-Descuentos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[6]/label[2]').text
-Bancos= 		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[7]/label[2]').text
-efectivo_egre= 	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[8]/label[2]').text
-T_bancos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[9]/label[2]').text
-T_efectivo=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[10]/label[2]').text
-Facebook= 		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[11]/label[2]').text
-Google=			driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[12]/label[2]').text
-Pasando=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[13]/label[2]').text
-Instagram=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[14]/label[2]').text
-Recomendado=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[15]/label[2]').text
-Link_agenda=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[16]/label[2]').text
-Sin_fuente=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[17]/label[2]').text
-C_perdidos=		driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[18]/label[2]').text
-C_recurrentes=	driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[19]/label[2]').text
-Cumple=			driver.find_element(By.XPATH,'//*[@id="InformacionDetallada"]/div[20]/label[2]').text
+	#"ventas","efectivo","Datafono","Transferencia","Egresos","Descuentos","Bancos","efectivo_egre","T_bancos","T_efectivo","Facebook","Google","Pasando","Instagram","Recomendado","Link_agenda","Sin_fuente","C_perdidos","C_recurrentes","Cumple"
+	labels=['fecha',"ventas","efectivo","Datafono","Transferencia","Egresos","Descuentos","Bancos","efectivo_egre","T_bancos","T_efectivo","Facebook","Google","Pasando","Instagram","Recomendado","Link_agenda","Sin_fuente","C_perdidos","C_recurrentes","Cumple"]
+	datos=[fecha,ventas,efectivo,Datafono,Transferencia,Egresos,Descuentos,Bancos,efectivo_egre,T_bancos,T_efectivo,Facebook,Google,Pasando,Instagram,Recomendado,Link_agenda,Sin_fuente,C_perdidos,C_recurrentes,Cumple]
 
 
-#"ventas","efectivo","Datafono","Transferencia","Egresos","Descuentos","Bancos","efectivo_egre","T_bancos","T_efectivo","Facebook","Google","Pasando","Instagram","Recomendado","Link_agenda","Sin_fuente","C_perdidos","C_recurrentes","Cumple"
-labels=["ventas","efectivo","Datafono","Transferencia","Egresos","Descuentos","Bancos","efectivo_egre","T_bancos","T_efectivo","Facebook","Google","Pasando","Instagram","Recomendado","Link_agenda","Sin_fuente","C_perdidos","C_recurrentes","Cumple"]
-datos=[ventas,efectivo,Datafono,Transferencia,Egresos,Descuentos,Bancos,efectivo_egre,T_bancos,T_efectivo,Facebook,Google,Pasando,Instagram,Recomendado,Link_agenda,Sin_fuente,C_perdidos,C_recurrentes,Cumple]
+	new_row = pd.Series({
+				"fecha":fecha,
+				"ventas":ventas,
+				"efectivo":efectivo,
+				"Datafono":Datafono,
+				"Transferencia":Transferencia,
+				"Egresos":Egresos,
+				"Descuentos":Descuentos,
+				"Bancos":Bancos,
+				"efectivo_egre":efectivo_egre,
+				"T_bancos":T_bancos,
+				"T_efectivo":T_efectivo,
+				"Facebook":Facebook,
+				"Google":Google,
+				"Pasando":Pasando,
+				"Instagram":Instagram,
+				"Recomendado":Recomendado,
+				"Link_agenda":Link_agenda,
+				"Sin_fuente":Sin_fuente,
+				"C_perdidos":C_perdidos,
+				"C_recurrentes":C_recurrentes,
+				"Cumple":Cumple,
+				})
+	return new_row,labels
 
-# for i in range(len(labels)):
-#     print(labels[i],datos[i])
-    
-file_exists = exists("Datos.csv")
+archivo="Datos.csv"
+def update_file(archivo):
 
-if not file_exists:
-    df=pd.DataFrame(columns=labels)
-    print(df)
+	new_row,labels=get_row(d1,fecha)
+		
 
-# print(datos)
-time.sleep(5000)
-driver.quit()
+	file_exists = exists(archivo)
+
+	if not file_exists:
+		df=pd.DataFrame(columns=labels)
+		# print(df)
+
+	if file_exists:
+		df=pd.read_csv(archivo)
+
+
+
+
+
+	if	fecha not in df.values :
+		print(f"fecha={fecha}")
+		df = df.append(new_row, ignore_index=True)
+		
+	else:
+		index=df.index
+		index_bool=(df["fecha"] == fecha)
+		cumplen=index[index_bool].to_list()
+		print(cumplen)
+		df.loc[cumplen[0]]=new_row
+		print("si esta")
+
+
+
+
+
+print(df)
+df.to_csv(archivo,index=False)
+
+
+
+
+
+
